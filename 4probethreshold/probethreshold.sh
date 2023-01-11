3bf6e7e98baacdfab48d7090b82af77cf72873cc
Lei Zhang <kinkikid2005@hotmail.com>
2023-01-11 23:40:15 +0800

    2023-1-11

--- 4probethreshold/probethreshold.sh
+++ 4probethreshold/probethreshold.sh
@@ -8,7 +8,7 @@ do
                         if  [[ $lbname == lb* ]]; then
                                 az network lb probe list --lb-name $lbname --resource-group $rgname --query '[].{probename:name }' -o tsv | while read -r probename
                                 do
-                                        echo "lbname is" $lbname "probe name is" $probename
+                                        echo "load balancer name is" $lbname "probe name is" $probename
                                         az network lb probe update -g $rgname --lb-name $lbname -n $probename --probe-threshold 3
                                 done 
                         fi           
