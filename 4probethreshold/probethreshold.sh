ff8b14e60d3673e25d7bd1a1c12179c55755f8b8
Lei Zhang <kinkikid2005@hotmail.com>
2023-01-11 23:39:19 +0800

    2023-1-11

--- 4probethreshold/probethreshold.sh
+++ 4probethreshold/probethreshold.sh
@@ -6,10 +6,10 @@ do
                 az network lb list -g $rgname --query '[].{lbname:name }' -o tsv | while read -r lbname
                 do 
                         if  [[ $lbname == lb* ]]; then
-                                echo $lbname
                                 az network lb probe list --lb-name $lbname --resource-group $rgname --query '[].{probename:name }' -o tsv | while read -r probename
                                 do
-                                             az network lb probe update -g $rgname --lb-name $lbname -n $probename --probe-threshold 3
+                                        echo "lbname is" $lbname "probe name is" $probename
+                                        az network lb probe update -g $rgname --lb-name $lbname -n $probename --probe-threshold 3
                                 done 
                         fi           
                 done                 
